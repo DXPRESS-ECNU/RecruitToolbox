f9f0df931fcdf4b0960853834ee7567b02ab9bce
Qucheng Miao <qcmiao@hotmail.com>
2019-08-22 23:20:45 +0800

    add project

---
+++ RecruitToolbox/ApplyForm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecruitToolbox
+{
+    class ApplyForm
+    {
+        public ApplyForm()
+        {
+            
+
+        }
+    }
+}
---
+++ RecruitToolbox/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RecruitToolbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
