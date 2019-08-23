cfced1e6de504c9b5101a2f978ab5d724a411058
Qucheng Miao <qcmiao@hotmail.com>
2019-08-23 14:55:04 +0800

    add Applicant class

---
+++ RecruitToolbox/Applicant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecruitToolbox
+{
+    class Applicant
+    {
+        public string Sid { get; set; }
+        public string Name { get; set; }
+        public string Tel { get; set; }
+        public string Mail { get; set; }
+        public string College { get; set; }
+        public string District { get; set; }
+        public string[] Applying { get; set; }
+        public string Resume { get; set; }
+    }
+}
