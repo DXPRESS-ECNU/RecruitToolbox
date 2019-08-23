ca26ee6a1191322f624dfb020fbd882080fecdb3
Qucheng Miao <qcmiao@hotmail.com>
2019-08-23 21:14:46 +0800

    Generate form

--- RecruitToolbox/Program.cs
+++ RecruitToolbox/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace RecruitToolbox
 {
@@ -6,8 +10,78 @@ namespace RecruitToolbox
     {
         static void Main(string[] args)
         {
-            ApplyForm.TemplatePdf = "formtemplate.pdf";
+            while (true)
+            {
+                Console.Write("1. Generate Applying Form PDF\n0. Exit\nChoose Function:  ");
+                string func = Console.ReadLine();
 
+                switch (func)
+                {
+                    case "0":
+                        return;
+                    case "1":
+                        GenerateForm();
+                        break;
+                }
+            }
+
+        }
+
+        static void GenerateForm()
+        {
+            GetCsv:
+            Console.Write("CSV File: ");
+            string applicantCsv = Console.ReadLine();
+            if (string.IsNullOrEmpty(applicantCsv) || !File.Exists(applicantCsv))
+            {
+                goto GetCsv;
+            }
+
+            IEnumerable<Applicant> applicants;
+            using (var reader = new StreamReader(applicantCsv))
+            using (var csv = new CsvReader(reader))
+            {
+                applicants = csv.GetRecords<Applicant>();
+            }
+
+            LoadTemplate:
+            Console.Write("Template Form File[formtemplate.pdf]: ");
+            string formTemplate = Console.ReadLine();
+            if (string.IsNullOrEmpty(formTemplate))
+            {
+                formTemplate = "formtemplate.pdf";
+            }
+            if (!File.Exists(formTemplate))
+            {
+                goto LoadTemplate;
+            }
+            ApplyForm.TemplatePdf = formTemplate;
+
+            LoadFormConfig:
+            Console.Write("Form Config File[formsettings.json]: ");
+            string formSettings = Console.ReadLine();
+            if (string.IsNullOrEmpty(formSettings))
+            {
+                formSettings = "formsettings.json";
+            }
+            if (!File.Exists(formSettings))
+            {
+                goto LoadFormConfig;
+            }
+
+            string settingJson = File.ReadAllText(formSettings);
+            FormSettings fs = JsonConvert.DeserializeObject<FormSettings>(settingJson);
+            ApplyForm.FormSettings = fs;
+
+            SavingPath:
+            Console.Write("PDF Saving Path: ");
+            string savingPath = Console.ReadLine();
+            if (string.IsNullOrEmpty(savingPath))
+            {
+                goto SavingPath;
+            }
+
+            ApplyForm.GeneratePdfForms(savingPath, applicants);
         }
     }
 }
